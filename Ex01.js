5f4f37311cbfacc96a1e98f9effb84229c68ecf6
JoksLobo <jocson_herry@hotmail.com>
2025-12-09 16:22:08 -0300

    commit inicial criação dos arquivos

---
+++ Ex01.js
@@ -0,0 +1,14 @@
+// Requisito 1: : Crie uma função que receba dois números inteiros e retorne a soma deles.
+// Função: somar
+// Exemplo Entrada: // somar(5, 3);
+// Exemplo Saída: // 8
+
+function somar(a, b) {
+  let soma = a + b;
+  console.log(soma);
+}
+
+let valor1 = 15;
+let valor2 = 20;
+
+somar(366, 235);
---
+++ Ex02.js
@@ -0,0 +1,15 @@
+const listaAlunos = [
+  "Ana",
+  "Carlos",
+  "Jaqueline",
+  "Estevão",
+  "Igor",
+  "Amanda",
+  "Zacarias",
+];
+
+function ordenarNomes(lista) {
+  return lista.sort();
+}
+
+console.log(ordenarNomes(listaAlunos));
