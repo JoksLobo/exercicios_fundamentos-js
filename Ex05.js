7caffdcdb7c20c611cef92103bff772e03c3e9fb
JoksLobo <jocson_herry@hotmail.com>
2025-12-09 16:49:40 -0300

    resolução exercicio 5

---
+++ Ex05.js
@@ -0,0 +1,8 @@
+function dividirFrase(frase) {
+  let fraseDividida = frase.split(" ");
+  return fraseDividida;
+}
+
+let frase = "Essa é uma frase de exemplo!";
+
+console.log(dividirFrase(frase));
